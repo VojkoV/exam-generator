1573161405d525378f90c97c3f5ec5c4e033270a
VojkoV <f.kirincic@student.liverpool.ac.uk>
2021-02-24 12:24:46 +0000

    port change

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.use(function (err, req, res, next) {
 
 
 // listen on port 3000
-app.listen(3000, function () {
-  console.log('Express app listening on port 3000');
+const PORT = process.env.PORT || 3000
+app.listen(PORT, function () {
+  console.log(`Express app listening on port ${PORT}`);
 });
